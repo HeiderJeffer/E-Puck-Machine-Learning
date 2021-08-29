d477aa5041179dc4175ab3dc913b00222476937c
unknown <hheider.jeffer@gmail.com>
2021-08-29 19:40:15 +0200

    Extend the Controller to Speed Control

--- controllers/my_controller_line_follower/my_controller_line_follower.py
+++ controllers/my_controller_line_follower/my_controller_line_follower.py
@@ -1,52 +1,21 @@
+from controller import Robot, Motor
 
-from controller import Robot,DistanceSensor,Motor
+TIME_STEP = 64
 
-TIME_STE=64
-MAX_SPEED=6.28
+MAX_SPEED = 6.28
+
+# create the Robot instance.
 robot = Robot()
-ps=[]
-psNames=[
-    'ps0','ps1','ps2','ps3',
-    'ps4','ps5','ps6','ps7'
-]
-for i in range(8):
-    ps.append(robot.getDevice(ps.Names[i]))
-    ps[i].enable(TIME_STEP)   
-leftMotor=robot.getDevices('left wheel motor')
-rightMotor=robot.getDevices('right wheel motor')    
-leftmotor.setPosition(float('inf'))
-rightmotor.setPosition(float('inf'))
-#set the speed to 10 % of the maximum speed
-leftmotor.setVelocity(0.0)
-rightmotor.setVelocity(0.0)
 
-# get the time step of the current world.
-timestep = int(robot.getBasicTimeStep())
+# get a handler to the motors and set target position to infinity (speed control)
+leftMotor = robot.getDevice('left wheel motor')
+rightMotor = robot.getDevice('right wheel motor')
+leftMotor.setPosition(float('inf'))
+rightMotor.setPosition(float('inf'))
+
+# set up the motor speeds at 10% of the MAX_SPEED.
+leftMotor.setVelocity(0.1 * MAX_SPEED)
+rightMotor.setVelocity(0.1 * MAX_SPEED)
 
 while robot.step(TIME_STEP) != -1:
-    # Read the sensors:
-    psValues=[]
-    for i in range(8):
-        psValues.append(ps[i].getValue())
-
-    right_obstacle=psValues[0]>80.0 or psValues[1]>80.0 or psValues[2]>80.0
-    left_obstacle=psValues[5]>80.0 or psValues[6]>80.0 or psValues[7]>80.0
-    leftSpeed=0.5*MAX_SPPED
-    rightSpeed=0.5*MAX_SPPED
-
-    
-    if left_obstacle:
-        leftSpeed= 0.5*MAX_SPEED
-        rightSpeed= -0.5*MAX_SPEED
-        
-     elif right_obstacle:
-            leftSpeed= -0.5 * MAX_SPEED
-            rightSpeed= 0.5 * MAX_SPEED   
-    
-    
-    leftMotor.setVelocity(leftSpeed)
-    pass
-
-
-my_new_controller.py
-Displaying my_new_controller.py.
+   pass
